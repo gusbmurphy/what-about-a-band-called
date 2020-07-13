0e6a9f1dc8f7809612cbfcda0a67022371ed9e3d
Gus Murphy <50927042+gusbmurphy@users.noreply.github.com>
2020-07-13 18:27:30 -0500

    Begin implementing RTK

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,16 +1,24 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
 import createSagaMiddleware from "redux-saga";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import * as reducers from "./reducers";
 import * as sagas from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
+const reduxLogger = createLogger();
+const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware, reduxLogger];
 
-export const store = createStore(
-  combineReducers(reducers),
-  applyMiddleware(createLogger(), sagaMiddleware)
-);
+export const store = configureStore({
+  reducer: combineReducers(reducers),
+  middleware: middleware,
+});
+
+// export const store = createStore(
+//   combineReducers(reducers),
+//   applyMiddleware(createLogger(), sagaMiddleware)
+// );
 
 for (let saga in sagas) {
   sagaMiddleware.run(sagas[saga]);
