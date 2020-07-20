30b4adc9b248b81a3b4ef38d51b9817de61acbba
Gus Murphy <50927042+gusbmurphy@users.noreply.github.com>
2020-07-20 13:26:09 -0500

    Convert statuses to enums

---
+++ src/app/store/statuses.ts
@@ -0,0 +1,43 @@
+export enum BandCreationStatuses {
+  CREATING,
+  CREATED,
+  ERROR,
+  BAND_EXISTS,
+  NOT_TRYING,
+}
+
+export enum BandSortTypes {
+  BEST,
+  WORST,
+  MOST_RECENT,
+}
+
+export enum BandScoreModificationStatuses {
+  ATTEMPTING,
+  SUCCESS,
+  FAILURE,
+  NOT_TRYING,
+}
+
+export enum AuthenticationStatuses {
+  AUTHENTICATING,
+  AUTHENTICATED,
+  NOT_AUTHENTICATED,
+  NOT_TRYING,
+  SERVER_ERROR,
+  USERNAME_NOT_FOUND,
+  INVALID_PASSWORD,
+  LOGGING_OUT,
+}
+
+export enum UserCreationStatuses {
+  PROCESSING,
+  PASSWORDS_DONT_MATCH,
+  USERNAME_TAKEN,
+  NOT_TRYING,
+  SERVER_ERROR,
+  SUCCESS,
+  EMPTY_FIELDS,
+  INVALID_EMAIL,
+  EMAIL_TAKEN,
+}
