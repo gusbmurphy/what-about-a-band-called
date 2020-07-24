357c5c2cf0524dd138d71b2f2a4e235ee2029ac1
Gus Murphy <50927042+gusbmurphy@users.noreply.github.com>
2020-07-24 14:23:49 -0500

    Add user record saga to saga index

--- src/app/store/sagas/index.js
+++ src/app/store/sagas/index.js
@@ -3,3 +3,4 @@ export { bandScoreModificationSaga } from "./band-score-modification-saga";
 export { userAuthenticationSaga } from "./user-authentication-saga";
 export { userCreationSaga } from "./user-creation-saga";
 export { watchFetchBandsSaga } from "./watch-fetch-bands-saga";
+export { watchFetchUserRecordsSaga } from "./fetch-user-records-saga";
