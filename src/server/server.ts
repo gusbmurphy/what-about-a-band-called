f39ccaad0b862e4a367d0708f7f7a7daca295e94
Gus Murphy <50927042+gusbmurphy@users.noreply.github.com>
2020-08-05 13:32:04 -0500

    Small server change

--- src/server/server.ts
+++ src/server/server.ts
@@ -40,7 +40,7 @@ app.use("/api/", apiLimiter);
 if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.resolve(__dirname, "../../dist")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("index.html"));
+    res.sendFile(path.resolve("dist/index.html"));
   });
 }
 
